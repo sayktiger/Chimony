6c1760189c354e46bf7acc7bfa72b4fda6316215
Konstantin <sayk.tiger09@gmail.com>
2022-09-25 20:22:28 +0300

    ready block news

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener(`DOMContentLoaded`, () => {
     const keys = new Swiper('.keys__swiper', {
         direction: 'horizontal',
-        loop: false,
+        loop: true,
         pagination: {
           el: '.keys__swiper-pagination',
         },
@@ -9,5 +9,37 @@ document.addEventListener(`DOMContentLoaded`, () => {
           nextEl: '.keys__swiper-button-next',
           prevEl: '.keys__swiper-button-prev',
         },
+        spaceBetween: 50,
+        speed: 800,
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+      }
+      });
+
+      const news = new Swiper('.news__swiper', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+          el: '.news__swiper-pagination',
+        },
+        navigation: {
+          nextEl: '.news__swiper-button-next',
+          prevEl: '.news__swiper-button-prev',
+        },
+        spaceBetween: 50,
+        speed: 800,
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+      }
       });
 });
